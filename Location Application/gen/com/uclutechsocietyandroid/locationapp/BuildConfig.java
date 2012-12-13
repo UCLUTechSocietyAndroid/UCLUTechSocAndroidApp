479992e66707c14e1d352318d626397505890a56
Hio Leong <hiowengleong@gmail.com>
2012-12-13 20:03:49 +0000

    Initial Commit

---
+++ Location Application/gen/com/uclutechsocietyandroid/locationapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.uclutechsocietyandroid.locationapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
